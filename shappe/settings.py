0705c763fd6ad32e43bcef5bb6710443618af87c
matsuihidetoshi <partyrockeverydayimshufflin@gmail.com>
2019-07-20 22:34:36 +0900

    changed cors setting

--- shappe/settings.py
+++ shappe/settings.py
@@ -54,12 +54,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware', 
+    
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    *,
-)
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'shappe.urls'
 
